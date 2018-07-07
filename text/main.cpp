34db689ad4e92cfe66f58e6ad7691e8cc85d53fa
fzk <fzk@admindeimac-32.local>
2018-07-07 15:13:02 +0800

    Initial Commit

---
+++ text/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  text
+//
+//  Created by fzk on 18/7/7.
+//  Copyright © 2018年 fzk. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
